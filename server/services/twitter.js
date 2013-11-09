dc6885a58ce7751dce6fa6d3fbcf31b3e1aa3ffd
Peter Johnson <insomnia@rcpt.at>
2013-11-09 06:04:30 +0000

    modify twitter stream filter

--- server/services/twitter.js
+++ server/services/twitter.js
@@ -33,7 +33,7 @@ function emit( label, tweet ) {
 
 var ev = new EventEmitter();
 client1.on( 'tweet', function ( tweet ) {
-  if( tweet.text.match( /Haiyan|Yolanda|Typhoon|Philippines|Filipina|Pontevedra|Flood|Airport|Rescue|Relief|Damage|Victim|Injured|Hurt|Help|Aftermath|Storm/i ) ){
+  if( tweet.text.match( /Haiyan|Yolanda|Typhoon|Filipina|Pontevedra|Flood|Airport|Rescue|Relief|Damage|Victim|Injured|Hurt|Help|Aftermath|Storm/i ) ){
 
     var geo;
     if( tweet.geo ) geo = tweet.geo.coordinates;
