cd9a2ee47724db3859d08071fef5eb87e44f859c
Peter Johnson <insomnia@rcpt.at>
2013-11-09 06:12:14 +0000

    modify twitter stream filter

--- server/services/twitter.js
+++ server/services/twitter.js
@@ -33,7 +33,7 @@ function emit( label, tweet ) {
 
 var ev = new EventEmitter();
 client1.on( 'tweet', function ( tweet ) {
-  if( tweet.text.match( /Haiyan|Yolanda|Typhoon|Filipina|Pontevedra|Flood|Safety|Airport|Rescue|Relief|Damage|Victim|Injured|Hurt|Help|Aftermath|Storm/i ) ){
+  if( tweet.text.match( /Haiyan|Yolanda|Typhoon|Filipina|Pontevedra|Flood|Survive|Safety|Airport|Rescue|Relief|Damage|Victim|Injured|Hurt|Help|Aftermath|Storm/i ) ){
 
     var geo;
     if( tweet.geo ) geo = tweet.geo.coordinates;
@@ -59,7 +59,7 @@ client1.on( 'tweet', function ( tweet ) {
 });
 
 client1.start(
-  ['Haiyan','Yolanda','Typhoon','Flood','Relief','Rescue','Damage','Victim','Philippines','Filipina','#YolandaPH','#Pontevedra'],
+  ['Haiyan','Yolanda','Typhoon','Flood','Relief','Rescue','Damage','Victim','#YolandaPH','#Pontevedra'],
   ['116.05957,5.441022','127.265625,19.601194']
 );
 // client1.start(null,['116.05957,5.441022','127.265625,19.601194']);
