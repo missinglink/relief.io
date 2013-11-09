2ba1bc82135d714e5fcb04f9712e2ab3a434791b
Peter Johnson <insomnia@rcpt.at>
2013-11-09 07:37:23 +0000

    highlight links in tweets

--- public/feature/map/IndexController.js
+++ public/feature/map/IndexController.js
@@ -45,7 +45,7 @@ app.controller( 'MapIndexController', function( $scope ) {
 
     var tweet = message.val();
     addTweet( tweet );
-    console.log( 'local', tweet );
+    // console.log( 'local', tweet );
   });
 
   // tweets.overseas.on( 'child_added', function( message ){
@@ -62,8 +62,45 @@ app.controller( 'MapIndexController', function( $scope ) {
     // $.flash(tweet.text);
     // console.log( 'push tweet', tweet.text );
     $scope.$apply( function(){
+      if( tweet.entities ){
+        if( Array.isArray( tweet.entities.user_mentions ) ){
+          for( var i=0; i<tweet.entities.user_mentions.length; i++ ){
+            tweet.text = tweet.text.replace( '@'+tweet.entities.user_mentions[i].screen_name, function( name ){
+              return '<a href="https://twitter.com/'+name.substr(1)+'" target="_new">'+name+'</a>';
+            });
+          }
+        }
+        if( Array.isArray( tweet.entities.hashtags ) ){
+          for( var i=0; i<tweet.entities.hashtags.length; i++ ){
+            tweet.text = tweet.text.replace( '#'+tweet.entities.hashtags[i].text, function( tag ){
+              return '<a href="https://twitter.com/search?q='+tag.substr(1)+'&src=typd" target="_new">'+tag+'</a>';
+            });
+          }
+        }
+        if( Array.isArray( tweet.entities.urls ) ){
+          for( var i=0; i<tweet.entities.urls.length; i++ ){
+            tweet.text = tweet.text.replace( tweet.entities.urls[i].url, function( link ){
+              return '<a href="'+link+'" target="_new">'+link+'</a>';
+            });
+          }
+        }
+        // console.log( JSON.stringify( tweet.entities.urls ) );
+      }
       $scope.tweets.push( tweet );
     });
+
+    // if( tweet.text.match( /yfrog|twitpic|twimg|twitter|img|pic/ ) ){
+    //   console.log( tweet.text );
+    // }
+
+    // if( tweet.entities ){
+      // console.log( 'tweet.entities', tweet.entities );
+      // if( tweet.entities.urls ){
+      //   console.log( tweet.entities.urls.map( function( url ){
+      //     return url.display_url;
+      //   }));
+      // }
+    // }
   });
 
 });
